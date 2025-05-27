ac7ac679cab5e4f2d9dcaf0fae690e6d4072f83f
SABAHAT <01-134241-041@student.bahria.edu.pk>
2025-05-27 22:59:17 +0500

    Add files via upload

---
+++ binarytree.cpp
@@ -0,0 +1,16 @@
+#include "binarytree.h"
+#include<iostream>
+
+using namespace std;
+int main() {
+	BinaryTree <int> t(1);
+	t.setLeft(t.getRoot(), 2);
+	t.setRight(t.getRoot(), 3);
+	t.setLeft(t.getRoot()->left, 4);
+	t.setRight(t.getRoot()->left, 5);
+	t.setLeft(t.getRoot()->right, 6);
+	t.setRight(t.getRoot()->right, 7);
+	// Preorder Traversal
+	t.preTrav();
+	return 0;
+}
